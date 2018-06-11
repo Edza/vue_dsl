c6bae15697e143ceec06320e19dd2e63046260eb
Eduards Vavere <eduards.vavere@accenture.com>
2018-06-11 16:53:47 +0300

    Progress works

--- src/dsl-compiler/common.ts
+++ src/dsl-compiler/common.ts
@@ -15,17 +15,30 @@ export class PraModItem {
     type = ''
 
     public setTreeViewParametersRecursive () {
+      this.type = this.isTechnical ? 'Tech' : 'Client'
+
+      if (this.children.length === 0) {
+        this.assignTextParameters()
+        return this.progress
+      } else {
+        let progressSum: number = 0
+
+        for (let child of this.children) {
+          progressSum += child.setTreeViewParametersRecursive()
+        }
+
+        this.progress = progressSum / this.children.length
+        this.assignTextParameters()
+        return this.progress
+      }
+    }
+
+    private assignTextParameters () {
       this.text = `${this.id}: ${this.about} | PROGRESS: ${Math.round(this.progress * 100)}%`
 
       this.description = `${this.id}: ${this.about} | PROGRESS: ${Math.round(this.progress * 100)}%\r\n`
       this.description += `TEHNISKĀ: ${this.isTechnical ? 'JĀ' : 'NĒ'}, KONFLIKTĒJOŠA: ${this.isConflicting ? 'JĀ' : 'NĒ'}\r\n`
       this.description += `ATBILDĪGS: ${this.responsible}, KOMANDA: ${this.assigned.join(', ')}`
-
-      this.type = this.isTechnical ? 'Tech' : 'Client'
-
-      for (let child of this.children) {
-        child.setTreeViewParametersRecursive()
-      }
     }
 
     constructor (id: number, about: string) {
